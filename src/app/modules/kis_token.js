bb48dab2b873d066014c140cd21a542924dac924
inhoo123 <a52169434@gmail.com>
2024-09-03 16:31:43 +0900

    api
    api 연동완료

--- src/app/modules/kis_token.js
+++ src/app/modules/kis_token.js
@@ -5,6 +5,38 @@ import {
   KIS_TOKEN_URL,
 } from "../config/kis_secret";
 
+// fetchToken
+const kisTokenFetchOption = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    grant_type: "client_credentials",
+    appkey: KIS_APP_KEY,
+    appsecret: KIS_APP_SECRET,
+  }),
+};
+
+const getToken = async () => {
+  try {
+    const queryString =
+      "https://openapivts.koreainvestment.com:29443/oauth2/tokenP";
+    const response = await fetch(queryString, kisTokenFetchOption);
+    const json = await response.json();
+
+    const accessToken = json.access_token;
+
+    console.log(accessToken);
+    return accessToken;
+  } catch (error) {
+    console.log("Error fetching token:", error);
+  }
+};
+export default getToken;
+//토큰을 발급 받는 함수
+
+/*
 const kisTokenFetchOption = {
   method: "POST",
   headers: {
@@ -41,4 +73,4 @@ const getToken = async () => {
   }
 };
 
-export default getToken;
+export default getToken;*/
