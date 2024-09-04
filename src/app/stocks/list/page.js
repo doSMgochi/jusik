64fabd07c257fd96f0b85b08465bf461d200b7da
inhoo123 <a52169434@gmail.com>
2024-09-04 16:08:33 +0900

    stashe
    asdfsdaf

--- src/app/stocks/list/page.js
+++ src/app/stocks/list/page.js
@@ -1,4 +1,22 @@
+import useStock from "@/app/modules/kis_stock_api";
+
 const ListPage = () => {
-  return <></>;
+  const stock = useStock();
+  return (
+    <table>
+      <thea>
+        <th>주식명</th>
+        <th>주식현재가</th>
+        <th>시가총액</th>
+        <th>주식코드</th>
+      </thea>
+      <tbody>
+        <td></td>
+        <td>{stock.stck_prpr}</td>
+        <td>{stock.hts_avls}</td>
+        <td>{stock.stck_shrn_iscd}</td>
+      </tbody>
+    </table>
+  );
 };
 export default ListPage;
