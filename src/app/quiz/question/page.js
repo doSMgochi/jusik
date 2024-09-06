91f3982ad1a0d807ac93c1acdccd5687814f77e4
inhoo123 <a52169434@gmail.com>
2024-09-06 16:42:55 +0900

    quiz
    route.js 랑 page.js 퀴즈 로직 짜는중

--- src/app/quiz/question/page.js
+++ src/app/quiz/question/page.js
@@ -1,10 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import axios from "axios";
+// import useStock from "@/app/modules/kis_stock_api";
+import { useSession, signIn } from "next-auth/react";
 
 const QuizPage = () => {
-  const [stocks, setStocks] = useState([]);
-  const [selectedStock, setSelectedStock] = useState(null);
+  // const { stock, loading, error } = useStock(selectedStock); // 주식실시간 정보
+  const [stocks, setStocks] = useState([]); //선택할 주식
+  const [selectedStock, setSelectedStock] = useState(null); //선택된 주식
   const [score, setScore] = useState(0); // 점수 상태를 추가
 
   useEffect(() => {
@@ -28,19 +32,32 @@ const QuizPage = () => {
   const getRandomData = (data, count) => {
     const shuffled = data.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
-  }; // random shuffle 근데이거 문자열 포함된 코드는 안넘어오는거같은데?
+  };
+
+  //주식정보를 불러오는 메세지
+  // if (loading) return <p>데이터를 불러오는 중입니다...</p>;
+  // if (error) return <p>오류: {error.message}</p>;
+  // if (!stock) return <p>주식정보가 없습니다.</p>;
 
   const handleStockClick = (stockIscd) => {
     setSelectedStock(stockIscd);
-    console.log("선택된 주식 코드:", stockIscd); // 선택된 주식 코드를 출력
 
+    try {
+      const userId = session.user.id;
+      const response = axios.post("/api/quiz", {
+        quiz_user_id: userId,
+      });
+    } catch (error) {
+      console.error("퀴즈데이터 보내는중 오류:", error);
+    }
+    console.log("선택된 주식 코드:", stockIscd); // 선택된 주식 코드를 출력
+    // 주식 코드를 보낼거임 api route에
     /* TODO: 퀴즈 로직 구현
-     1. 정답 판별
+     1. 정답 판별!!
      2. 점수 계산
      3. setScore를 통해 점수 업데이트
      4. 필요하다면 새로운 문제 출제 (fetchStocks 호출 또는 stocks 상태 업데이트)
      */
-    ////////
   };
 
   const viewRandomStocks = stocks.map((stock) => (
