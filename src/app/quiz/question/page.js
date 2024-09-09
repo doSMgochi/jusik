d85b0ac4ee708af00d847d44f97561a171900706
inhoo123 <a52169434@gmail.com>
2024-09-09 09:16:25 +0900

    quiz
    작업중

--- src/app/quiz/question/page.js
+++ src/app/quiz/question/page.js
@@ -8,7 +8,7 @@ import { useSession, signIn } from "next-auth/react";
 const QuizPage = () => {
   const [stocks, setStocks] = useState([]); // DB선택할 주식
   const [selectedStock, setSelectedStock] = useState(null); // 선택된 주식
-  const {stock, loading, error } = useStock(selectedStock); // API 선택된주식 실시간 정보
+  const { stock, loading, error } = useStock(selectedStock); // API 선택된주식 실시간 정보
   const [score, setScore] = useState(0); //  점수 상태를 추가
 
   useEffect(() => {
@@ -45,22 +45,21 @@ const QuizPage = () => {
     //근대 두개를 보냈는데 한개씩 비교를 어떻게하지?
     //그냥 에초에 db에 stockISCD로 그거를 이름이름이랑 시총을 if로 바로 처리한뒤 둘중하나 높은곳을 맞추면 점수상승으로하고
     //틀린거클릭하면 예외처리를 하면되겠다.
-    if(stocks.map[0]===stockIscd){
-      
-    } 
-    console.log("시가총액",stock.hts_avls);
+    // if (stocks.map[0] === stockIscd) {
+    // }
+    // console.log("시가총액", stock.hts_avls);
     // 왜토큰오류가 뜨지?
-      /* 여기서 정답이 맞으면 setScore를 1씩 상승시킬거임
+    /* 여기서 정답이 맞으면 setScore를 1씩 상승시킬거임
        <p>현재가: {stock.stck_prpr}원</p> 이걸 2개 비교 if 해서 점수 올리거나 틀리면 스코어 setScore를 멈추고
        DB에 삽입해야함
-      */    
+      */
 
     try {
       const userId = session.user.id; //세션으로 quiz 아이디를 보내서 저장하고
       const response = axios.post("/api/quiz", {
-      //quiz_no는 auto_increment 긴한데 삽입해야되나?
+        //quiz_no는 auto_increment 긴한데 삽입해야되나?
         quiz_user_id: userId, //O
-        quiz_collect: quizScore,//X 아직  로직을 다처리한다음 테이블에 삽입할거임
+        quiz_collect: quizScore, //X 아직  로직을 다처리한다음 테이블에 삽입할거임
       });
     } catch (error) {
       console.error("퀴즈데이터 보내는중 오류:", error);
@@ -92,8 +91,7 @@ const QuizPage = () => {
         <h3>둘중 누가더 시가총액이 클까!</h3>
         <ul>{viewRandomStocks}</ul>
       </div>
-      <div className="quiz result">
-      </div>
+      <div className="quiz result"></div>
     </>
   );
 };
