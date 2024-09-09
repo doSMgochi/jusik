47c9cd794f01689dc74ab0630abee78eda1331d2
jinh0318a <jinh0318a@naver.com>
2024-09-09 10:35:44 +0900

    stock-favorite

---
+++ src/app/api/favorite/user/route.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request) {
+  const url = new URL(request.url);
+  const user_id = url.searchParams.get("user_id");
+
+  if (!user_id) {
+    return new Response(JSON.stringify({ error: "사용자 ID가 필요합니다." }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const favorites = await prisma.tbl_favorites.findMany({
+      where: { f_user_id: user_id },
+      include: { tbl_stocks: true }, // 주식 정보 포함
+    });
+
+    console.log("Favorites fetched:", favorites); // 데이터 확인
+    return new Response(JSON.stringify(favorites), { status: 200 });
+  } catch (error) {
+    console.error("즐겨찾기 조회 중 오류 발생:", error);
+    return new Response(
+      JSON.stringify({ error: "즐겨찾기 조회 중 오류가 발생했습니다." }),
+      { status: 500 }
+    );
+  }
+}
